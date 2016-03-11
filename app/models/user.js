f3c1ef47320d718ed7ac42b5101c1cfb42f1e6cc
Flavia Pittet <pittet.flavia@gmail.com>
2016-03-11 14:44:53 +0100

    models

---
+++ app/models/user.js
@@ -0,0 +1,33 @@
+/**
+ * Created by flaviapittet on 11/03/16.
+ */
+
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    type: String,
+    name: String,
+    lastname: String,
+    email: String,
+    password: String,
+    passed_levels: [
+        {
+            level_id: {
+                type: Schema.Types.ObjectId,
+                required: false,
+                ref: "Level"
+            }
+        }
+
+    ]
+});
+
+/*UserSchema.virtual('date')
+ .get(function(){
+ return this._id.getTimestamp();
+ });*/
+
+mongoose.model('User', UserSchema);
+
