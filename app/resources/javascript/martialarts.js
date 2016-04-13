e4db9d2e7eecdf60f71474fa5cb371abe4e303b6
yunis.darbella <yunis.darbellay@heig-vd.ch>
2016-04-13 11:07:58 +0200

    développement du premier kata

--- app/resources/javascript/martialarts.js
+++ app/resources/javascript/martialarts.js
@@ -1,35 +1,56 @@
 // Attente du document ready
 $(document).ready(function () {
-    //Définition des variables
-    var intro = 0;
 
+  //Définition des variables
+  var intro = 0;
 
-    $(".martialArtsNextBtn").on('click', introCount);
+  //detection du clic sur suivant
+  $(".martialArtsNextBtn").on('click', introCount);
 
-    function introCount() {
-        intro++;
-        if (intro === 3) {
-            console.log("suivant clicked " + intro + " fois");
-            $(kata1);
-        }
+  //Fonction liée au clic sur suivant
+  function introCount() {
+    //Incrémentation de la variable au clic sur le bouton suivant
+    intro++;
+    //Dès que l'on a clické assez de fois sur suivant...
+    if (intro === 3) {
+      console.log("suivant clicked " + intro + " fois");
+      //...On lance la fonction gérant le premier kata
+      $(kata1);
     }
+  }
 
 });
 
+//Fonction gérant le premier kata
 function kata1() {
 
-    //Variable de la fonction
-    var tblKata1 = ['Poing', 'Pied', 'Poing'];//Premier kata préconfiguré
-    var tblInputs=[];//Tableau stockant les inputs pour le kata
-    var nbrInputKata1 = 0;//Nbr d'inputs que le user a entré
+  //Variable propre à la fonction
+  var tblKata1 = ['btnPoing', 'btnPied', 'btnPoing'];//Premier kata préconfiguré
+  var tblInputs=[];//Tableau stockant les inputs pour le kata
+  var nbrInputKata1 = 0;//Nbr d'inputs que le user a entré
+  var i = 0;
 
-    //Détéction des inputs
-    $('#btnPoing, #btnPied, #btnSpe').click(function () {
+  //Détéction des inputs
+  $('#btnPoing, #btnPied, #btnSpe').click(function () {
 
-            var i = 0;
-            nbrInputKata1++;
-            console.log("Vous avez clické sur " + nbrInputKata1 + " inputs");
-            tblInputs[i] = this.id;
+    //Nombre d'input saisi
+    nbrInputKata1++;
 
-    });
+    //Injection des valeurs saisies dans un tableau
+    tblInputs[i] = this.id;
+    i++;
+    console.log(tblInputs);
+
+    //Dès que le bon nombre d'inputs est saisi...
+    if (nbrInputKata1 === 3) {
+      //On compare nos tableau
+      var is_same = tblKata1.length == tblInputs.length && tblKata1.every(function(element, index) {
+        return element === tblInputs[index];
+
+      });
+      console.log(is_same);
+
+    }
+
+  });
 }
