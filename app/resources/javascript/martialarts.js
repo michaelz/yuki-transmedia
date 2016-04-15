b5bd35b04c65aa34562e36e4b3dd85a8841c63de
yunis.darbella <yunis.darbellay@heig-vd.ch>
2016-04-15 09:33:48 +0200

    automatisation de fonction javascript

--- app/resources/javascript/martialarts.js
+++ app/resources/javascript/martialarts.js
@@ -2,19 +2,26 @@
 $(document).ready(function () {
 
   //Définition des variables
+  ///////////////////////////////////////////////////////////////
   var intro = 0;
-    $('#martialArtsBubble2').hide();
-    $('#martialArtsBubble3').hide();
-    $('#martialArtsBubble4').hide();
+
+  //Masquage des éléments
+  $('#martialArtsBubble2').hide();
+  $('#martialArtsBubble3').hide();
+  $('#martialArtsBubble4').hide();
+  $('.martialArtsBtn').hide();
+
 
   //detection du clic sur suivant
   $(".martialArtsNextBtn").on('click', introCount);
 
-  //Fonction liée au clic sur suivant
+
+  //Fonction liée au dialogue
+  ///////////////////////////////////////////////////////////////
   function introCount() {
     //Incrémentation de la variable au clic sur le bouton suivant
     intro++;
-
+    //Masquage/affichage de ligne de dialogue en fonction de l'avancement du script
     if (intro === 1) {
       $('#martialArtsBubble1').hide();
       $('#martialArtsBubble2').show();
@@ -24,58 +31,82 @@ $(document).ready(function () {
       $('#martialArtsBubble2').hide();
       $('#martialArtsBubble3').show();
     }
-
-    //Dès que l'on a clické assez de fois sur suivant...
+    //Dernière ligne de dialogue, dès que suivant on lance une fonction...
     if (intro === 3) {
+      //Masquage du sript et du bouton next
       $('#martialArtsBubble3').hide();
       $('.martialArtsNextBtn').hide();
-      console.log("suivant clicked " + intro + " fois");
-      //...On lance la fonction gérant le premier kata
-      $(kata1);
+      //Affichage des inputs du jeu
+      $('.martialArtsBtn').show();
+      //...On lance la fonction gérant les katas
+      $(kata);
     }
   }
 
 });
 
-//Fonction gérant le premier kata
-function kata1() {
+//Fonction gérant les katas
+///////////////////////////////////////////////////////////////
+function kata() {
   $('#martialArtsBubble4').show();
   //Variable propre à la fonction
-  var tblKata1 = ['btnPoing', 'btnPied', 'btnPoing'];//Premier kata préconfiguré
+  var tblKata = [];//Premier kata préconfiguré
   var tblInputs=[];//Tableau stockant les inputs pour le kata
-  var nbrInputKata1 = 0;//Nbr d'inputs que le user a entré
-  var i = 0;
+  var nbrInputKata = 0;//Nbr d'inputs que le user a entré
+  var i = 0;//Index pour le kata aléatoire définir le kata
+  var j = 0;//Index pour les inputs saisi
+  var lngKata;
 
-  //Détéction des inputs
-  $('#btnPoing, #btnPied, #btnSpe').click(function () {
+  //Génération du kata aléatoire
+  while (i<3) {
+    var aleatoireNbr = Math.round(Math.random()*3) + 1;
+    if (aleatoireNbr === 1) {
+      tblKata[i] = "btnPoing";
+      i++;
+    } else if (aleatoireNbr === 2) {
+      tblKata[i] = "btnPied";
+      i++;
+    } else if (aleatoireNbr === 3) {
+      tblKata[i] = "btnJump";
+      i++;
+    } else if (aleatoireNbr === 4) {
+      tblKata[i] = "btnSpe";
+      i++;
+    }}
 
-    //Nombre d'input saisi
-    nbrInputKata1++;
-
-    //Injection des valeurs saisies dans un tableau
-    tblInputs[i] = this.id;
-    i++;
-    console.log(tblInputs);
-
-    //Dès que le bon nombre d'inputs est saisi...
-    if (nbrInputKata1 === 3) {
-      //On compare nos tableau
-      var is_same = tblKata1.length == tblInputs.length && tblKata1.every(function(element, index) {
-        return element === tblInputs[index];
-      });
-      console.log(is_same);
-      if (is_same === true) {
-        console.log("Bien joué le kata a été effectué à la perfection");
-        kata2(tblInputs);
-      } else {
-        console.log("Dommage tu as fait une erreur, recommençons");
-        kata1();
-      }
+    if (i === 3) {
+        var nbrKata = 1;
+      $( "#nbrKata" ).append(nbrKata);
     }
-  });
-}
 
-//Fonction gérant le second kata
-function kata2() {
-  console.log("kata2");
-}
+    console.log(tblKata);
+
+    //Détéction des inputs
+    $('#btnPoing, #btnPied, #btnJump, #btnSpe').click(function () {
+
+      //Nombre d'input saisi
+      nbrInputKata++;
+
+      //Injection des valeurs saisies dans un tableau
+      tblInputs[j] = this.id;
+      j++;
+      console.log(tblInputs);
+
+      //Dès que le bon nombre d'inputs est saisi...
+      if (nbrInputKata === 3) {
+        //On compare nos tableau
+        var is_same = tblKata.length == tblInputs.length && tblKata.every(function(element, index) {
+          return element === tblInputs[index];
+        });
+        console.log(is_same);
+        if (is_same === true) {
+          console.log("Bien joué le kata a été effectué à la perfection");
+          lngKata = lngKata++;
+          console.log(lngKata);
+        } else {
+          console.log("Dommage tu as fait une erreur, recommençons");
+          kata();
+        }
+      }
+    });
+  }
