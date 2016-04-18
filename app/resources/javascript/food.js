90cf25bc6dcdd57a6327a45158e8ee8ed04e9aae
Flavia Pittet <pittet.flavia@gmail.com>
2016-04-18 16:27:03 +0200

    food.js

--- app/resources/javascript/food.js
+++ app/resources/javascript/food.js
@@ -4,7 +4,7 @@ $.getJSON("/api/quiz", function (data) {
     var length = data.length;
     var positionUser = 0;
     console.log(length);
-    $(".question").append(data[0].question);
+    $(".question").append(data[positionUser].question);
     $.each(data[0].answers, function (a, answer) {
         $(".choices").append("<li class='" + a + "'>" + answer.text + "</li>");
 
@@ -13,22 +13,34 @@ $.getJSON("/api/quiz", function (data) {
     $(".choices li").click(function () {
         var pos = $(this).attr("class");
 
-        var selectedAnswer = data[0].answers[pos].is_solution;
+        var selectedAnswer = data[positionUser].answers[pos].is_solution;
         if (selectedAnswer) {
             //ajouter un bouton next
             //envoyer en post la validation a l'API à la fin
             //message de confirmation
             $(".button").empty();
-            $(".button").append('<button type="button" class="true"> Send</button>');
-           /* if(positionUser = length){
 
-            }*/
+
+            if(positionUser == length){
+
+                $(".button").append('<button type="button" class="true"> <a href="">Terminer</a></button>');
+
+            } else{
+                $(".button").append('<button type="button" class="true"> <a href="#">Suivant</a></button>');
+                positionUser++;
+
+                $(".true").click(function(){
+                    console.log("position User: " + positionUser);
+                });
+
+            }
 
         } else {
             //message d'erreur
             //bouton recommencer
             $(".button").empty();
-            $(".button").append('<button type="button" class="false"> Start again</button>');
+            $(".button").append('<button type="button" class="false">Recommencer</button>');
+
         }
     });
 
