7299dc09d897d1e5292f1d988ff740ba5077f593
Flavia Pittet <pittet.flavia@gmail.com>
2016-04-18 14:43:51 +0200

    jquery quiz

--- app/resources/javascript/food.js
+++ app/resources/javascript/food.js
@@ -1,14 +1,32 @@
-$.getJSON( "/api/quiz", function( data ) {
-    console.log(data[0]);
+$.getJSON("/api/quiz", function (data) {
+    console.log(data);
+
+    var length = data.length;
+    console.log(length);
     $(".question").append(data[0].question);
-    $.each(data[0].answers, function(a,answer){
-        console.log(answer);
-        $(".choices").append("<li class='"+a+"'>"+answer.text+"</li>");
+    $.each(data[0].answers, function (a, answer) {
+        $(".choices").append("<li class='" + a + "'>" + answer.text + "</li>");
+
 
     });
-    $( ".choices li" ).click(function() {
+    $(".choices li").click(function () {
         var pos = $(this).attr("class");
-        console.log(pos);
+
+        var selectedAnswer = data[0].answers[pos].is_solution;
+        if (selectedAnswer) {
+            //ajouter un bouton next
+            //envoyer en post la validation a l'API à la fin
+            //message de confirmation
+            $(".button").empty();
+            $(".button").append('<button type="button" class="true"> Send</button>');
+
+        } else {
+            //message d'erreur
+            //bouton recommencer
+            $(".button").empty();
+            $(".button").append('<button type="button" class="false"> Start again</button>');
+        }
     });
+
 
 });
