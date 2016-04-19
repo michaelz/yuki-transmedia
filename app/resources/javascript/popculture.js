8b8f708a922f106c7c2fb057ccc1d1f2b302a745
emmanuel schenk <Emmanuel@mbp-de-emmanuel.einet.ad.eivd.ch>
2016-04-19 13:17:20 +0200

    ajout image et amélioration du css et du js

--- app/resources/javascript/popculture.js
+++ app/resources/javascript/popculture.js
@@ -1,8 +1,25 @@
 /*
  * POP CULTURE
  */
+ 
+$(".menuFonds").hide();
 
-console.log("this is popculture");
-    $(function() {
-    $(".drag").draggable();
-});
+$(".drag").draggable({ containment: "#main", scroll: false });
+
+$(".btnAccessoires").click( function () {
+	$(".menuAccessoires").show();
+	$(".menuFonds").hide();
+})
+
+$(".btnFonds").click( function () {
+	$(".menuAccessoires").hide();
+	$(".menuFonds").show();
+})
+
+$("#fondsImg1").click( function () {
+	$('#contenu').css("background-image", "url(/img/png1.jpg)");
+})
+
+$("#fondsImg2").click( function () {
+	$('#contenu').css("background-image", "url(/img/3.jpg)");
+})
