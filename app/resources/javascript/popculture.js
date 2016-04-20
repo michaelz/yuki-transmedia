bd06ea39b04aa5cc971286e4dce9d3c29b796b6d
emmanuel schenk <Emmanuel@mbp-de-emmanuel.einet.ad.eivd.ch>
2016-04-20 12:44:38 +0200

    correction du bug drag and drop, maintenant l'accessoire dnd change de div et se positionne correctement

--- app/resources/javascript/popculture.js
+++ app/resources/javascript/popculture.js
@@ -5,8 +5,26 @@
 $(".menuFonds").hide();
 
 $(".drag").draggable({ 
-	containment: "#main", 
-	scroll: false});
+	containment: "#main"});
+
+$("#droppableContenu").droppable({
+	drop: function(event, ui) {
+        var position = $("#" + ui.draggable[0].id).position();
+        console.log($("#droppableContenu").height());
+        $("#droppableContenu").append(ui.draggable[0]);
+        console.log(position);
+        console.log($("#droppableContenu").height());
+        console.log($("#" + ui.draggable[0].id).height() * 0.5);
+        console.log();
+        $("#" + ui.draggable[0].id).css({'position':'absolute', 'left': position.left + 'px', 'top': position.top + 'px' });       
+	}
+});
+
+$("#droppableMenu").droppable({
+	drop: function(event, ui) {
+        $("#droppableMenu").append(ui.draggable[0]);
+    }
+});
 
 $(".btnAccessoires").click( function () {
 	$(".menuAccessoires").show();
@@ -20,26 +38,28 @@ $(".btnFonds").click( function () {
 
 
 $("#fondsImg1").click( function () {
-	if ($('#contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/png1.jpg")') {
-		$('#contenu').css("background-image", "");
+	if ($('.contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/png1.jpg")') {
+		$('.contenu').css("background-image", "");
 	} else {
-		$('#contenu').css("background-image", "url(/img/png1.jpg)");
+		$('.contenu').css("background-image", "url(/img/png1.jpg)");
 	}
 })
 
 $("#fondsImg2").click( function () {
-	if ($('#contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/3.jpg")') {
-		$('#contenu').css("background-image", "");
+	if ($('.contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/3.jpg")') {
+		$('.contenu').css("background-image", "");
 	} else {
-		$('#contenu').css("background-image", "url(/img/3.jpg)");
+		$('.contenu').css("background-image", "url(/img/3.jpg)");
 	}
 })
 
 $("#boutton").click( function () {
-	html2canvas( [ document.body ], {
-	          onrendered: function(canvas) {
-	          	alert("sd");
-	            document.body.appendChild(canvas);
-	          }
-	});
+	html2canvas( $('.contenu'), {
+	        onrendered: function(canvas) {
+	 		var a = document.createElement('a');
+	        // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+	        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+	        a.download = 'somefilename.jpg';
+	        a.click();
+	}});
 })
