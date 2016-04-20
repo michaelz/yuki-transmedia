6227c3fe87ad4642dbcb378926e0b181fd85031c
Michael Zwyssig <mzwyssig@3ne.ch>
2016-04-20 17:27:47 +0200

    food new logic and added bg image and a bit of styling

--- app/resources/javascript/food.js
+++ app/resources/javascript/food.js
@@ -1,51 +1,102 @@
-$.getJSON("/api/quiz", function (data) {
-    console.log(data);
-
-    var length = data.length;
-    var positionUser = 0;
-    console.log(length);
+var pos = 0; // define starting question
 
 
-
-    $(".question").append(data[positionUser].question);
-    $.each(data[positionUser].answers, function (a, answer) {
-        $(".choices").append("<li class='" + a + "'>" + answer.text + "</li>");
-
-
+/**
+ *
+ */
+$.getJSON("/api/quiz", function(data) {
+  if (data.length > 0) {
+    // Build questions
+    $.each(data, function(q, question) {
+        buildQuestion(q, question);
     });
-    $(".choices li").click(function () {
-        var pos = $(this).attr("class");
+    $('#startQuiz').on('click', function() {
+        console.log('click');
+        $(this).parent().hide(); // hide the startQuiz button
+        startQuiz(data);
+    });
+} else {
+    $('#startQuiz').parent().append('<p>Pas de questions ici, désolé !</p>');
+}
+});
 
-        var selectedAnswer = data[positionUser].answers[pos].is_solution;
-        if (selectedAnswer) {
-            //ajouter un bouton next
-            //envoyer en post la validation a l'API à la fin
-            //message de confirmation
-            $(".button").empty();
 
+/*
+ * Start the Quiz function
+ * data should be the data received from the ajax call
+ */
+var startQuiz = function(data) {
+    $('.q-'+pos).show(); // show first question
 
-            if(positionUser == length){
-
-                $(".button").append('<button type="button" class="true"> <a href="">Terminer</a></button>');
-
-            } else{
-                $(".button").append('<button type="button" class="true"> <a href="#">Suivant</a></button>');
-                positionUser++;
-
-                $(".true").click(function(){
-                    console.log("position User: " + positionUser);
-                });
-
-            }
-
-        } else {
-            //message d'erreur
-            //bouton recommencer
-            $(".button").empty();
-            $(".button").append('<button type="button" class="false">Recommencer</button>');
-
+    $('.answer').on('click', function() {
+        if (data[pos].answers[$(this).attr('data-position')].is_solution) {
+            nextQuestion(pos, data.length);
         }
     });
+}
 
 
-});
+/*
+ * Next question or finish quiz if question is last
+ */
+var nextQuestion = function(currentPos, total) {
+    if (currentPos == total) {
+        $(".q-"+pos).hide();
+        $('.quiz').append('ok !');
+    } else {
+        $(".q-"+pos).hide();
+        pos++;
+        $(".q-"+pos).show();
+    }
+}
+
+/**
+ * Fonction to build the frontend of the questionBlock
+ */
+var buildQuestion = function(pos, q) {
+  // var length = data.length;
+  // Template is hidden by default;
+  var qTpl = '<div style="display:none" class="questionBlock q-'+pos+'" data-question="'+pos+'"><ol class="choices"></ol></div>';
+
+  $(".quiz").append(qTpl);
+  $(".q-"+pos).prepend('<h3 class="qTitle">'+q.question+'</h3>');
+
+  $.each(q.answers, function(a, answer) {
+    $(".q-"+pos+" .choices").append("<li data-position='"+a+"' class='answer'>" + answer.text + "</li>");
+  });
+}
+
+
+/*$(".choices li").click(function() {
+  var pos = $(this).attr("data-position");
+
+  var selectedAnswer = q.answers[pos].is_solution;
+  if (selectedAnswer) {
+    //ajouter un bouton next
+    //envoyer en post la validation a l'API à la fin
+    //message de confirmation
+    $(".button").empty();
+
+/*
+    if (positionUser == length) {
+
+      $(".button").append('<button type="button" class="true"> <a href="">Terminer</a></button>');
+
+    } else {
+      $(".button").append('<button type="button" class="true"> <a href="#">Suivant</a></button>');
+      positionUser++;
+
+      $(".true").click(function() {
+        console.log("position User: " + positionUser);
+      });
+
+  }*//*
+
+  } else {
+    //message d'erreur
+    //bouton recommencer
+    $(".button").empty();
+    $(".button").append('<button type="button" class="false">Recommencer</button>');
+
+  }
+});*/
