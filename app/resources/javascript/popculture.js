531e1f840528a89170585fc9d1348a20ca28253c
emmanuel schenk <Emmanuel@mbp-de-emmanuel.einet.ad.eivd.ch>
2016-04-21 16:34:02 +0200

    la route upload pour l'image fonctionne et opérationnel, de nouveau accessoire sont ajouté. il reste un bug à corriger au niveau des accessoires, ils n'ont pas tous la taille désirée.

--- app/resources/javascript/popculture.js
+++ app/resources/javascript/popculture.js
@@ -1,59 +1,71 @@
 /*
  * POP CULTURE
  */
- 
-$(".menuFonds").hide();
 
-$(".drag").draggable({ 
-	containment: "#main"});
-
-$("#droppableContenu").droppable({
-	drop: function(event, ui) {
-        var position = $("#" + ui.draggable[0].id).position();
-        $("#droppableContenu").append(ui.draggable[0]);
-        $("#" + ui.draggable[0].id).css({'position':'absolute', 'left': position.left + 'px', 'top': position.top + 'px' });       
+$( document ).ready(function() {
+	for (var i = 1; i < 18; i++) {
+		$(".menuAccessoires").append('<div class="drag" id="' + i + '"><img src="img/jeu_popculture/accessoires/item' + i +'.png" class="accessoire" /></div>');
 	}
-});
 
-$("#droppableMenu").droppable({
-	drop: function(event, ui) {
-        $("#droppableMenu").append(ui.draggable[0]);
-        $("#" + ui.draggable[0].id).css({'position':'relative', 'left': '', 'top':'' }); 
-    }
-});
-
-$(".btnAccessoires").click( function () {
-	$(".menuAccessoires").show();
 	$(".menuFonds").hide();
-})
 
-$(".btnFonds").click( function () {
-	$(".menuAccessoires").hide();
-	$(".menuFonds").show();
-})
+	$(".drag").draggable({ 
+		containment: "#main"});
+
+	$("#droppableContenu").droppable({
+		drop: function(event, ui) {
+	        var position = $("#" + ui.draggable[0].id).position();
+	        $("#droppableContenu").append(ui.draggable[0]);
+	        $("#" + ui.draggable[0].id).css({'position':'absolute', 'left': position.left + 'px', 'top': position.top + 'px' });       
+		}
+	});
+
+	$("#droppableMenu").droppable({
+		drop: function(event, ui) {
+	        $("#droppableMenu").append(ui.draggable[0]);
+	        $("#" + ui.draggable[0].id).css({'position':'relative', 'left': '', 'top':'' }); 
+	    }
+	});
+
+	$(".btnAccessoires").click( function () {
+		$(".menuAccessoires").show();
+		$(".menuFonds").hide();
+	})
+
+	$(".btnFonds").click( function () {
+		$(".menuAccessoires").hide();
+		$(".menuFonds").show();
+	})
 
 
-$("#fondsImg1").click( function () {
-	if ($('.contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/png1.jpg")') {
-		$('.contenu').css("background-image", "");
-	} else {
-		$('.contenu').css("background-image", "url(/img/png1.jpg)");
-	}
-})
+	$("#fondsImg1").click( function () {
+		if ($('.contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/jeu_popculture/fonds/arts-martiaux.png")') {
+			$('.contenu').css("background-image", "");
+		} else {
+			$('.contenu').css("background-image", "url(/img/jeu_popculture/fonds/arts-martiaux.png)");
+		}
+	})
 
-$("#fondsImg2").click( function () {
-	if ($('.contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/3.jpg")') {
-		$('.contenu').css("background-image", "");
-	} else {
-		$('.contenu').css("background-image", "url(/img/3.jpg)");
-	}
-})
+	$("#fondsImg2").click( function () {
+		if ($('.contenu').css("background-image") == 'url("http://' + $(location).attr('host') + '/img/jeu_popculture/fonds/calligraphie.png")') {
+			$('.contenu').css("background-image", "");
+		} else {
+			$('.contenu').css("background-image", "url(/img/jeu_popculture/fonds/calligraphie.png)");
+		}
+	})
+	
+});
 
 function capture() {
-	html2canvas( $('.contenu'), {
+	$("#photoBouton").hide();
+	html2canvas($('.contenu'), {
 	        onrendered: function(canvas) {
-		 		$("#img_val").val(canvas.toDataURL("image/png"));
-		        // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
-		        document.getElementById("myForm").submit();
+		 		$("#img").val(canvas.toDataURL("image/png"));
+		 		var a = document.createElement('a');
+		 		a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+		        a.download = 'somefilename.jpg';
+		        a.click();
+		        $("#photoBouton").show();
+		 		document.getElementById("myForm").submit();
 	}});
 }
