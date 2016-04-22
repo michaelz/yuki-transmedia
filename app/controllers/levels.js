1b0ae1b49648c108504ec8ac63b26fd1ed8e85e2
Flavia Pittet <pittet.flavia@gmail.com>
2016-04-22 09:11:53 +0200

    levels passing

--- app/controllers/levels.js
+++ app/controllers/levels.js
@@ -114,57 +114,50 @@ router.put("/:id", function (req, res, next) {
 /**
  * Pass a level
  */
-router.post('/passLevelUser/:idLevel/', tools.verifyToken, function (req, res, next) {
+router.post('/passLevelUser/:idLevel/', auth.mustBeAuthenticated, auth.getUserInfo, function (req, res, next) {
 
-    var user = User.findById(req.idUser, function (err, user) {
+
+    user = req.connectedUser;
+
+
+    console.log(user);
+
+    var level = Level.findById(req.params.idLevel, function (err, level) {
         if (err) {
             res.status(500).send(err);
             return;
-        } else if (!user) {
-            res.status(404).send('User not found');
-            return;
+        } else if (!level) {
+            res.status(404).send("level not found");
         }
 
+        var newResult = "";
 
-        console.log(user);
+        if (req.body.result) {
+            newResult = req.body.result;
 
-        var level = Level.findById(req.params.idLevel, function (err, level) {
+        }
+        //assign level and result
+
+
+        var newInput = {
+            "level_id": req.params.idLevel,
+            "result": newResult
+        };
+
+        user.passed_levels.push(newInput);
+
+
+        user.save(function (err, updatedUser) {
             if (err) {
                 res.status(500).send(err);
                 return;
-            } else if (!level) {
-                res.status(404).send("level not found");
             }
-
-            var newResult = "";
-
-            if (req.body.result) {
-                newResult = req.body.result;
-
-            }
-            //assign level and result
-
-
-            var newInput = {
-                "level_id": req.params.idLevel,
-                "result": newResult
-            };
-
-            user.passed_levels.push(newInput);
-
-
-            user.save(function (err, updatedUser) {
-                if (err) {
-                    res.status(500).send(err);
-                    return;
-                }
-                res.send(updatedUser);
-            });
-
-
-
+            res.send(updatedUser);
         });
+
+
     });
+
 });
 
 /*
