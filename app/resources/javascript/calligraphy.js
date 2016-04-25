d5bbd86b8bf2fa15022b6bd169cec1f2efe4393a
Michael Zwyssig <mzwyssig@3ne.ch>
2016-04-25 15:17:02 +0200

    modifs infobox et divers

--- app/resources/javascript/calligraphy.js
+++ app/resources/javascript/calligraphy.js
@@ -3,7 +3,7 @@ var settings = {
     cols: 3, // number of columns [3 ... 9]
     hole: 9, // initial hole position [1 ... rows*columns]
     shuffle: true, // initially show shuffled pieces [true|false]
-    numbers: false, // initially show numbers on pieces [true|false]
+    numbers: true, // initially show numbers on pieces [true|false]
     language: 'fr', // language for gui elements [language code]
 
     // display additional gui controls
@@ -11,7 +11,7 @@ var settings = {
         shufflePieces: false, // display 'Shuffle' button [true|false]
         confirmShuffle: false, // ask before shuffling [true|false]
         toggleOriginal: true, // display 'Original' button [true|false]
-        toggleNumbers: true, // display 'Numbers' button [true|false]
+        toggleNumbers: false, // display 'Numbers' button [true|false]
         counter: true, // display moves counter [true|false]
         timer: true, // display timer (seconds) [true|false]
         pauseTimer: true // pause timer if 'Original' button is activated
--- app/resources/javascript/map.js
+++ app/resources/javascript/map.js
@@ -1,15 +1,22 @@
 // TODO Appel aux webservices
 $(document).ready(function() {
     $('.round-info-button').hide();
-  
+
     var levels = [{
-        code: 'martialarts'
+        code: 'martialarts',
+        url: 'mondes/artsmartiaux'
     }, {
-        code: 'calligraphy'
+        code: 'calligraphy',
+        url: 'mondes/calligraphie'
+
     }, {
-        code: 'popculture'
+        code: 'popculture',
+        url: 'mondes/popculture'
+
     }, {
-        code: 'food'
+        code: 'food',
+        url: 'mondes/nourriture'
+
     }]; // TODO: Replace with data from user (API)
 
     levels.forEach(function(data) {
@@ -17,35 +24,39 @@ $(document).ready(function() {
         var item = $('.item-' + data.code);
 
         item.removeClass('disabled').append("<a href='/" + data
-            .code + "'>" + item.val() + "</a>");
+            .url + "'>" + item.val() + "</a>");
     });
-var slideIndex = 1;
-showDivs(slideIndex);
+    var slideIndex = 1;
+    showDivs(slideIndex);
 
-$(".plus-one").click(function() {
-  slideIndex += 1;
-  showDivs(slideIndex);
-});
+    $(".plus-one").click(function() {
+        slideIndex += 1;
+        showDivs(slideIndex);
+    });
 
-$(".minus-one").click(function() {
-  slideIndex -= 1;
-  showDivs(slideIndex);
-});
+    $(".minus-one").click(function() {
+        slideIndex -= 1;
+        showDivs(slideIndex);
+    });
 
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        if (n > x.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = x.length
+        };
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
     }
-    x[slideIndex-1].style.display = "block";
-}
 
-function validate(){
+    function validate() {
 
-}
+    }
     //$(".element:not(.item-intro)").append("<div class='indice'><select><option value='volvo'>Volvo</option><option value='saab'>Saab</option><option value='mercedes'>Mercedes</option> <option value='audi'>Audi</option></select></div>");
 });
