6f8012574d8d8717c55633fdb6afe40735c8bc91
emmanuel schenk <Emmanuel@mbp-de-emmanuel.einet.ad.eivd.ch>
2016-04-26 10:37:21 +0200

    Modification du model level pour rendre les champs number, date et requis unique lors du chargement

--- app/models/level.js
+++ app/models/level.js
@@ -5,10 +5,10 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var LevelSchema = new Schema({
-    number: Number,
+    number: {type: Number, unique: true, required: true},
     name: String,
     code: String,
-    release_date: Date,
+    release_date: {type: Date, unique: true, required: true},
     additional_info: String,
     description: String,
     clue: {
