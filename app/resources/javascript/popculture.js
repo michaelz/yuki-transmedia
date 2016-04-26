505f2a844ed236bffc109b96537fe92cd88c2f38
Michael Zwyssig <mzwyssig@3ne.ch>
2016-04-26 14:19:43 +0200

    popculture css redone

--- app/resources/javascript/popculture.js
+++ app/resources/javascript/popculture.js
@@ -52,6 +52,8 @@ $(document).ready(function() {
             $(".dialogue").fadeOut();
             //Lancer votre fonction post dialogue ici...
             yukiCustomisation(nbMove, lvl);
+            adjustHeight();
+            $('.square').addClass('game');
         }
     }
 
@@ -74,103 +76,142 @@ $(document).ready(function() {
 
     $('.round-info-button').on('click', showInfos);
 
-});
+
+
+    /*
+     * POP CULTURE Game
+     */
+
+    function yukiCustomisation() {
+        $('.round-info-button').fadeOut();
+        $('#main').fadeIn();
+        for (var i = 1; i < 18; i++) {
+            $(".menuAccessoires").append('<div class="drag" id="img' +
+                i +
+                '"><img src="/img/jeu_popculture/accessoires/item' +
+                i +
+                '.png" class="accessoire" ></div>');
+        }
+
+        //$(".menuFonds").hide();
+
+        $(".drag").draggable({
+            containment: "#main"
+        });
+
+        $("#droppableContenu").droppable({
+            drop: function(event, ui) {
+                var position = $("#" + ui.draggable[0].id).position();
+                $("#droppableContenu").append(ui.draggable[
+                    0]);
+                $("#" + ui.draggable[0].id).css({
+                    'position': 'absolute',
+                    'left': position.left + 'px',
+                    'top': position.top + 'px'
+                });
+            }
+        });
+
+        $("#droppableMenu").droppable({
+            drop: function(event, ui) {
+                $("#droppableMenu").append(ui.draggable[0]);
+                $("#" + ui.draggable[0].id).css({
+                    'position': 'relative',
+                    'left': '',
+                    'top': ''
+                });
+            }
+        });
+
+        /*$(".btnAccessoires").click(function() {
+            $(".btnAccessoires").css("color", "#3498db");
+            $(".btnAccessoires").css("background", "white");
+
+            $(".btnFonds").css("color", "white");
+            $(".btnFonds").css("background", "#3498db");
+
+            $(".btnFonds").css("border-top-width", "6px");
+            $(".btnFonds").css("border-bottom-width", "0px");
+
+            $(".menuAccessoires").show();
+            $(".menuFonds").hide();
+        })*/
+
+        /*$(".btnFonds").click(function() {
+            $(".btnFonds").css("color", "#3498db");
+            $(".btnFonds").css("background", "white");
+
+            $(".btnFonds").css("border-top-width", "0px");
+            $(".btnFonds").css("border-bottom-width", "6px");
+
+            $(".btnAccessoires").css("color", "white");
+            $(".btnAccessoires").css("background", "#3498db");
+
+            $(".menuFonds").show();
+            $(".menuAccessoires").hide();
+        })*/
+
+
+        $("#fondsImg1").click(function() {
+            if ($('.contenu').css("background-image") ==
+                'url("http://' + $(
+                    location).attr('host') +
+                '/img/jeu_popculture/fonds/arts-martiaux.png")'
+            ) {
+                $('.contenu').css("background-image", "");
+            } else {
+                $('.contenu').css("background-image",
+                    "url(/img/jeu_popculture/fonds/arts-martiaux.png)"
+                );
+            }
+        })
+
+        $("#fondsImg2").click(function() {
+            if ($('.contenu').css("background-image") ==
+                'url("http://' + $(
+                    location).attr('host') +
+                '/img/jeu_popculture/fonds/calligraphie.png")') {
+                $('.contenu').css("background-image", "");
+            } else {
+                $('.contenu').css("background-image",
+                    "url(/img/jeu_popculture/fonds/calligraphie.png)"
+                );
+            }
+        })
+    };
 
 
 
-/*
- * POP CULTURE Game
- */
+    function adjustHeight() {
+        var width = $('.square-element').width();
+        $('.square-element').css('height', width + 'px');
+        $('#purikura').css('height', width + 'px');
+        //$('#menu').css('height', width + 'px');
 
-function yukiCustomisation() {
-    $('.round-info-button').fadeOut();
-    $('#main').fadeIn();
-    for (var i = 1; i < 18; i++) {
-        $(".menuAccessoires").append('<div class="drag" id="img' + i +
-            '"><img src="/img/jeu_popculture/accessoires/item' + i +
-            '.png" class="accessoire" ></div>');
     }
 
-    $(".menuFonds").hide();
-
-    $(".drag").draggable({
-        containment: "#main"
+    // calls adjustHeight on window load
+    jQuery(window).load(function() {
+        adjustHeight();
     });
 
-    $("#droppableContenu").droppable({
-        drop: function(event, ui) {
-            var position = $("#" + ui.draggable[0].id).position();
-            $("#droppableContenu").append(ui.draggable[0]);
-            $("#" + ui.draggable[0].id).css({
-                'position': 'absolute',
-                'left': position.left + 'px',
-                'top': position.top + 'px'
-            });
-        }
+    // calls adjustHeight anytime the browser window is resized
+    jQuery(window).resize(function() {
+        adjustHeight();
     });
 
-    $("#droppableMenu").droppable({
-        drop: function(event, ui) {
-            $("#droppableMenu").append(ui.draggable[0]);
-            $("#" + ui.draggable[0].id).css({
-                'position': 'relative',
-                'left': '',
-                'top': ''
-            });
-        }
-    });
+    function showInfos() {
+        $('.infoBox').show();
+        $('.round-info-button').hide();
+        $('.round-info-button-close').on('click', hideInfos);
+    }
 
-    $(".btnAccessoires").click(function() {
-        $(".btnAccessoires").css("color", "#3498db");
-        $(".btnAccessoires").css("background", "white");
+    function hideInfos() {
+        $('.round-info-button').show();
+        $('.infoBox').hide();
 
-        $(".btnFonds").css("color", "white");
-        $(".btnFonds").css("background", "#3498db");
-
-        $(".btnFonds").css("border-top-width", "6px");
-        $(".btnFonds").css("border-bottom-width", "0px");
-
-        $(".menuAccessoires").show();
-        $(".menuFonds").hide();
-    })
-
-    $(".btnFonds").click(function() {
-        $(".btnFonds").css("color", "#3498db");
-        $(".btnFonds").css("background", "white");
-
-        $(".btnFonds").css("border-top-width", "0px");
-        $(".btnFonds").css("border-bottom-width", "6px");
-
-        $(".btnAccessoires").css("color", "white");
-        $(".btnAccessoires").css("background", "#3498db");
-
-        $(".menuFonds").show();
-        $(".menuAccessoires").hide();
-    })
-
-
-    $("#fondsImg1").click(function() {
-        if ($('.contenu').css("background-image") == 'url("http://' + $(
-                location).attr('host') +
-            '/img/jeu_popculture/fonds/arts-martiaux.png")') {
-            $('.contenu').css("background-image", "");
-        } else {
-            $('.contenu').css("background-image",
-                "url(/img/jeu_popculture/fonds/arts-martiaux.png)");
-        }
-    })
-
-    $("#fondsImg2").click(function() {
-        if ($('.contenu').css("background-image") == 'url("http://' + $(
-                location).attr('host') +
-            '/img/jeu_popculture/fonds/calligraphie.png")') {
-            $('.contenu').css("background-image", "");
-        } else {
-            $('.contenu').css("background-image",
-                "url(/img/jeu_popculture/fonds/calligraphie.png)");
-        }
-    })
-};
+    }
+});
 
 function capture() {
     $("#photoBouton").hide();
@@ -186,16 +227,4 @@ function capture() {
             document.getElementById("myForm").submit();
         }
     });
-}
-
-function showInfos() {
-    $('.infoBox').show();
-    $('.round-info-button').hide();
-    $('.round-info-button-close').on('click', hideInfos);
-}
-
-function hideInfos() {
-    $('.round-info-button').show();
-    $('.infoBox').hide();
-
 }
