7329886e64455818bd70eaab4f6282fdb241f093
Michael Zwyssig <mzwyssig@3ne.ch>
2016-04-26 18:37:10 +0200

    add level tweaks

--- app/resources/javascript/formLevel.js
+++ app/resources/javascript/formLevel.js
@@ -9,21 +9,36 @@ $(document).ready(function() {
     //Permet de prendre l'id dans l'url
     var sPageURL = window.location.toString();
     var sURLid = sPageURL.split('/');
-    id = sURLid[sURLid.length-1];
+    id = sURLid[sURLid.length - 1];
 
     //Controle si l'id existe pour faire le get pour modifier un niveau
     if (id) {
-        $.get( "/api/level/" + id)
+        $.get("/api/level/" + id)
             .done(function(data) {
                 $("#code").val(data.code);
                 $("#date_sortie").val(data.release_date);
                 $("#url").val(data.url);
             })
             .fail(function() {
-                console.log( "error" );
-        })
+                console.log("error");
+            })
     }
+    $.datetimepicker.setLocale('fr');
+    $('#date_sortie').datetimepicker({
+        format: 'YYYY-MM-DD\THH:mm',
+        formatDate: 'YYYY-MM-DD HH:mm',
+        mask: false,
+        datepicker: true,
+        timepicker: true
+    });
 });
+
+Date.parseDate = function(input, format) {
+    return moment(input, format).toDate();
+};
+Date.prototype.dateFormat = function(format) {
+    return moment(this).format(format);
+};
 
 //Function envoyant les données des formulaires (créer un level, modifier un level) pour créer ou modifier un level
 function capture() {
@@ -35,22 +50,22 @@ function capture() {
     //Sélectionne la bonne root pour le bon formulaire
     if (id == "ajoutMonde") {
         $.ajax({
-          type: "POST",
-          url: "/api/level",
-          data: level
-        }).done(function (data) {
+            type: "POST",
+            url: "/api/level",
+            data: level
+        }).done(function(data) {
             window.location.replace('/admin');
-        }).fail(function (err){
+        }).fail(function(err) {
             console.log(err);
         });
     } else {
         $.ajax({
-          type: "PUT",
-          url: "/api/level/" + id,
-          data: level
-        }).done(function (data) {
+            type: "PUT",
+            url: "/api/level/" + id,
+            data: level
+        }).done(function(data) {
             window.location.replace('/admin');
-        }).fail(function (err){
+        }).fail(function(err) {
             console.log(err);
         });
     }
