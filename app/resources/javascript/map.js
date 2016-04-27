e865f9499725ae808306016909da2251f097ada5
Michael Zwyssig <mzwyssig@3ne.ch>
2016-04-27 13:57:31 +0200

    indices visibles

--- app/resources/javascript/map.js
+++ app/resources/javascript/map.js
@@ -6,51 +6,31 @@ $(document).ready(function() {
         '<source class="audioSource" src="/audio/carte/65SecNatureSoundsEnchantedForest-TheHonestGuys.mp3" type="audio/mpeg">'
     );
 
-    // Get activated levels
+    /**
+     * Work with active levels (depending on dates)
+     */
     $.get('/api/level/active').done(function(levels) {
         levels.forEach(function(data) {
             var itemSelector = '.item-' + data.code;
             $(itemSelector).removeClass('disabled');
             $(itemSelector + ' .icon').attr("href", '/' +
                 data.url);
+            $(itemSelector + ' .sign-indice').show();
         });
     }).fail(function(err) {
 
     });
 
 
-    // Get passed levels
+    /**
+     * Work with passed levels to show indices
+     */
     $.get('/api/user/me').done(function(data) {
-        /*
-        var levels = [{
-            code: 'martialarts',
-            url: 'mondes/artsmartiaux',
-            solved: true
-        }, {
-            code: 'calligraphy',
-            url: 'mondes/calligraphie',
-            solved: false
-        }, {
-            code: 'popculture',
-            url: 'mondes/popculture',
-            solved: true
-
-        },  {
-            code: 'origami',
-            url: 'mondes/origamis',
-            solved: true
-
-        },{
-            code: 'food',
-            url: 'mondes/nourriture',
-            solved: true
-
-        }]; */
         var levels = data.data.passed_levels;
-
+        console.log(levels);
         levels.forEach(function(data) {
             var itemSelector = '.item-' + data.code;
-            $(itemSelector + ' .sign-indice').show();
+            $(itemSelector).addClass("solved");
         });
     }).fail(function(err) {
         console.log(err);
